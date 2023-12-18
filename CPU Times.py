9b83b3d3e51a17f6a2f754d1e482ec7623208350
Jason Dallas <146230423+daljas@users.noreply.github.com>
2023-12-17 19:27:49 -0500

    Update CPU Times.py

--- CPU Times.py
+++ CPU Times.py
@@ -1,3 +1,11 @@
+# Script Name:                  CPU Times Information Script
+# Author:                       Jason Dallas
+# Date of latest revision:      12/23/2023
+# Purpose:                      Display CPU times information
+
+
+
+
 import psutil
 
 # Get CPU times information
@@ -13,3 +21,5 @@ print(f"Hardware Interrupt Time: {cpu_times.irq} seconds")
 print(f"Software Interrupt Time: {cpu_times.softirq} seconds")
 print(f"Virtualized OS Time: {cpu_times.steal} seconds")
 print(f"Guest OS CPU Time: {cpu_times.guest} seconds")
+
+
