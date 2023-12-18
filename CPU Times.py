0f26acb55e02f37c1eb122c1bbed24a574807d81
Jason Dallas <146230423+daljas@users.noreply.github.com>
2023-12-17 19:26:52 -0500

    Create CPU Times.py

---
+++ CPU Times.py
@@ -0,0 +1,15 @@
+import psutil
+
+# Get CPU times information
+cpu_times = psutil.cpu_times()
+
+# Print the obtained information
+print(f"User Mode Time: {cpu_times.user} seconds")
+print(f"Kernel Mode Time: {cpu_times.system} seconds")
+print(f"Idle Time: {cpu_times.idle} seconds")
+print(f"Priority User Mode Time: {cpu_times.nice} seconds")
+print(f"I/O Wait Time: {cpu_times.iowait} seconds")
+print(f"Hardware Interrupt Time: {cpu_times.irq} seconds")
+print(f"Software Interrupt Time: {cpu_times.softirq} seconds")
+print(f"Virtualized OS Time: {cpu_times.steal} seconds")
+print(f"Guest OS CPU Time: {cpu_times.guest} seconds")
